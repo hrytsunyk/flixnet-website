515f3d871dd3fb92b2d6233373442874999f52a3
serhiihrytsunyk <serhii.hrytsunyk@gmail.com>
2023-04-26 02:59:20 +0300

    fix video

--- src/components/VideoListCard/VideoListCard.js
+++ src/components/VideoListCard/VideoListCard.js
@@ -2,13 +2,18 @@ import React from 'react';
 import 'mui-player/dist/mui-player.min.css'
 import MuiPlayer from 'mui-player'
 import {urls} from "../../configs";
+// import css from "../MovieInfo/MovieInfo.module.css";
+import css from './Video.module.css';
 
 const VideoListCard = ({video}) => {
     const {name,key, type} = video;
 
 
     return (
-        <div>
+        <div className={css.VideoListCardFather}>
+
+            <div className={css.hrLine}></div>
+
          <iframe
              width="800"
              height="500"
@@ -18,6 +23,8 @@ const VideoListCard = ({video}) => {
          >
 
          </iframe>
+            <div className={css.hrLineBottom}></div>
+
         </div>
     );
 };
